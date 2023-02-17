1c957d67b79acb8c263b916e1169afbc7b3a1882
galaraz <jvictor.si.2013@gmail.com>
2023-02-17 11:43:40 -0400

    update com vite

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import {nanoid} from 'nanoid';
 
-const randomPassword = nanoid()
-console.log(randomPassword)
+
+const passwordbtnEl = document.querySelector('button')
+
+
+const displayPasswordEl = document.querySelector('h2')
+
+
+passwordbtnEl.addEventListener('click', () =>{
+    const randomPassword = nanoid()
+    displayPasswordEl.innerHTML = randomPassword
+})
